a9fa2e7f5154e21c6599d6effcb11958b1a1ce94
Emiel Steerneman <emielsteerneman@gmail.com>
2023-06-23 20:31:09 +0200

    update

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 # save this as app.py
 from flask import Flask, render_template, request, send_from_directory
-import db
+from Database import instance as db_instance
 import utilities as U
+from embeddings import Embeddor as E
 
 app = Flask(__name__, template_folder='templates')
+
+E.set_sentences(db_instance.get_sentences())
 
 @app.route('/TDPs/<year>/<filename>')
 def download_file(year, filename):
     return send_from_directory(f'TDPs/{year}', filename, as_attachment=True)
 
+@app.route('/templates/<filename>')
+def static_file(filename):
+    return send_from_directory(f'templates', filename)
+
+
 def tdps(request, groupby=None):
-    tdps = db.get_tdps()
+    tdps = db_instance.get_tdps()
     tdps = [ dict(_) for _ in tdps ]
     
     if groupby == 'team':
@@ -18,30 +26,83 @@ def tdps(request, groupby=None):
         for tdp in tdps:
             if tdp['team'] not in teams: teams[tdp['team']] = []
             teams[tdp['team']].append(tdp)
-        # Sort each team's TDPs by year
-        # for team in teams:
-        #     teams[team] = sorted(teams[team], key=lambda x: x['year'])
-            
+        # Sort by year
         teams = { team: sorted(teams[team], key=lambda _: _['year'], reverse=True) for team in teams }
         return render_template('tdps.html', data=teams, groupby=groupby)
     
+    if groupby == 'year':
+        years = {}
+        for tdp in tdps:
+            if tdp['year'] not in years: years[tdp['year']] = []
+            years[tdp['year']].append(tdp)
+        # Sort by team
+        years = { year: sorted(years[year], key=lambda _: _['team']) for year in years }
+        return render_template('tdps.html', data=years, groupby=groupby)
+
     return render_template('tdps.html', data=tdps, groupby=groupby)
 
 @app.get("/")
 def hello():
-    return "Hello, World! <br>" + "<br>".join(U.find_all_TDPs())
+    return tdps(request)
 
 @app.get("/tdps/<id>")
 def get_tdps_id(id):
-    tdp = dict(db.get_tdp(id))
+    tdp = dict(db_instance.get_tdp(id))
     filepath = f"/TDPs/{tdp['year']}/{tdp['filename']}"
     return render_template('tdp.html', tdp=tdp, filepath=filepath)
 
+# @app.get("/api/tdps/<id>")
+# def get_api_tdps_id(id):
+#     return { "hello": "world" }
+
+@app.get("/api/tdps")
+def get_api_tdps():
+    return db_instance.get_tdps()
+
+
+""" /api/tdps/<tdp_id>/paragraphs """
+
+@app.get("/api/tdps/<tdp_id>/paragraphs")
+def get_api_tdps_id_paragraphs(tdp_id):
+    return db_instance.get_paragraphs(tdp_id)
+@app.post("/api/tdps/<tdp_id>/paragraphs")
+def post_api_tdps_id_paragraphs(tdp_id):
+    body = request.get_json()
+    paragraph_id, tdp_id, title, text = body['id'], body['tdp_id'], body['title'], body['text']
+    db_instance.post_paragraph(paragraph_id, tdp_id, title, text)
+
+    sentences, embeddings = U.paragraph_to_sentences_embeddings(text)
+    db_instance.post_sentences(paragraph_id, sentences, embeddings)
+
+    return db_instance.get_paragraphs(tdp_id)
+@app.delete("/api/tdps/<tdp_id>/paragraphs/<paragraph_id>")
+def delete_api_tdps_id_paragraphs(tdp_id, paragraph_id):
+    db_instance.delete_paragraph(paragraph_id)
+    return db_instance.get_paragraphs(tdp_id)
+
+""" /api/query """
+
+@app.post("/api/query")
+def post_api_query():
+    body = request.get_json()
+    query = body['query']
+    sentences, paragraphs, tdps = E.query(query)
+    return {
+        'sentences': sentences,
+        'paragraphs': paragraphs,
+        'tdps': tdps
+    }
+
+
+
+
 @app.post("/tdps/<id>")
 def post_tdps_id(id):
-    print("\n\n")
-    print("POST", id)
     print(request.form)
+    
+    # Store text from field 'textfield' in database
+    text = request.form.get('textfield')
+    db_instance.post_text(id, text)
     
     return get_tdps_id(id)
 
@@ -50,19 +111,3 @@ def get_tdps():
     groupby = request.args.get('groupby')
     return tdps(request, groupby)
 
-# Add api route to add new tdp to database
-@app.post("/tdps")
-def tdps_post():
-    # Get dataa from request
-    filename = request.form.get('filename')
-    team = request.form.get('team')
-    year = request.form.get('year')
-    is_etdp = request.form.get('is_etdp')    
-    
-    print(is_etdp)
-    
-    # db.add_tdp(filename, team, year, is_etdp)
-    return tdps(request)
-
-db = db.DB()
-print(db)
--- utilities.py
+++ utilities.py
@@ -1,6 +1,7 @@
 import os
 import utilities as U
-import db
+from Database import instance as db_instance
+from embeddings import Embeddor as E
 
 def find_all_TDPs():
     """Find all TDP pdf files in all subdirectories of current directory"""
@@ -29,12 +30,97 @@ def find_all_tdps_and_add_to_database(db):
     for tdp in parsed:
         db.add_tdp(tdp['filename'], tdp['team'], tdp['year'], tdp['is_etdp'])
     
+def paragraph_to_sentences_embeddings(paragraph):
+    sentences = paragraph.split('.')
+    embeddings = E.embed(sentences)
+    return sentences, embeddings
+
+def resentence_paragraphs():
+    print("[resentence_paragraphs] Resentencing paragraphs...")
+
+    # Report total number of sentences
+    sentences = db_instance.get_sentences()
+    print(f"[resentence_paragraphs] {len(sentences)} sentences found")
+
+    tdps = db_instance.get_tdps()
+    print(f"[resentence_paragraphs] {len(tdps)} TDPs found")
+    for tdp in tdps:
+        print(f"[resentence_paragraphs] Resentencing TDP {tdp['id']} : {tdp['team']} {tdp['year']}")
+        paragraphs = db_instance.get_paragraphs(tdp['id'])
+        for paragraph in paragraphs:
+            print(f"[resentence_paragraphs]     Resentencing paragraph {paragraph['id']} : {paragraph['title']}")
+            sentences, embeddings = paragraph_to_sentences_embeddings(paragraph['text'])
+            db_instance.post_sentences(paragraph['id'], sentences, embeddings)
+
+    # Report total number of sentences
+    sentences = db_instance.get_sentences()
+    print(f"[resentence_paragraphs] {len(sentences)} sentences found")
+
+
+def first_test():
+    print("Importing packages...")
+    from sentence_transformers import SentenceTransformer
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    print("Loading model...")
+    model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+
+    cosine_similarity = lambda a, b: np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+    paragraph_embeddings = []
+
+    # Get paragraphs from tdp id 63
+    paragraphs = db_instance.get_paragraphs(63) + db_instance.get_paragraphs(213)
+    for paragraph in paragraphs:
+        print(paragraph['title'].rjust(40), "|", paragraph['text'][:100])
+        sentences = paragraph['text'].split('.')
+        embeddings = model.encode(sentences)
+        paragraph_embeddings.append({
+            'title': paragraph['title'],
+            'embedding_average': np.average(embeddings, axis=0),
+            'embeddings': embeddings,
+            'sentences': sentences,
+            'tdp_id': paragraph['tdp_id']
+        })
+        
+
+    queries = [
+        "I want to know more about ROS and how it works",
+        "I want to know more about the wheels",
+        "I want to know more about wireless communication between robots",
+        "I want to know more about simulation",
+        "How does grSim work?",
+        "Who created grSim?",
+    ]
+
+    for query in queries:
+
+        print()
+        print(query)
+        query_embedding = model.encode(query)
+        
+        # Distance between query and average of paragraph embeddings
+        distances = [ [cosine_similarity(query_embedding, p['embedding_average']), p] for p in paragraph_embeddings ]
+        distances = sorted(distances, reverse=True)
+        for distance, p in distances[:5]:
+            print(f"{p['tdp_id']}".rjust(5), p['title'].rjust(40), f"{distance:.3f}")
+
+        # Find best matching sentences
+        distances_sentences = []
+        for p in paragraph_embeddings:
+            for sentence, embedding in zip(p['sentences'], p['embeddings']):
+                distances_sentences.append([cosine_similarity(query_embedding, embedding), sentence, p])
+        distances_sentences = sorted(distances_sentences, key=lambda _: _[0], reverse=True)
+        print()
+        for distance, sentence, p in distances_sentences[:5]:
+            print(f"{p['tdp_id']}".rjust(5), p['title'].rjust(40), f"{distance:.3f}", sentence[:150])
+
 if __name__ == "__main__":
     
-    db = db.DB()
-    
-    find_all_tdps_and_add_to_database(db)
+    # first_test()
+    # db = db.DB()
+    # find_all_tdps_and_add_to_database(db)
+
+    resentence_paragraphs()
         
-    rows = [ dict(_) for _ in db.get_tdps() ]
-    teams = list(set( [ _['team'] for _ in rows ] ))
-    print(sorted(teams))
