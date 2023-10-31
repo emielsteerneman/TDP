cce0966eaf0f926435a278cbcf074f603c0fd548
Emiel Steerneman <emielsteerneman@gmail.com>
2023-10-31 20:21:35 +0100

    Added function to convert PDFs to HTML

--- utilities.py
+++ utilities.py
@@ -61,6 +61,34 @@ def resentence_paragraphs():
     print(f"[resentence_paragraphs] {len(sentences)} sentences found")
 
 
+def pdfs_to_html():
+    """Convert all TDP pdf files to html files"""
+    tdps = db_instance.get_tdps()
+    print(tdps[0])
+     
+    PDFTOHTML_FOLDER_NAME = "pdftohtml_folder"
+        
+    command = ["pdftohtml", "-c", "-s", "-dataurls"]
+    for i_tdp_db, tdp_db in enumerate(tdps):
+        try:
+            # Create folder
+            os.makedirs(PDFTOHTML_FOLDER_NAME, exist_ok=True)
+            print(f"Converting {i_tdp_db}/{len(tdps)} {tdp_db.filename}...")
+            filepath = f"TDPs/{tdp_db.year}/{tdp_db.filename}"
+            shutil.copyfile(filepath, f"{PDFTOHTML_FOLDER_NAME}/{tdp_db.filename}")
+            
+            subprocess.call(command + [f"{PDFTOHTML_FOLDER_NAME}/{tdp_db.filename}"], stdout=subprocess.DEVNULL)
+            
+            # wer.pdf is converted to wer-html.html. Copy to static/tdps/id/tdp.html
+            filename_html_in = f"{PDFTOHTML_FOLDER_NAME}/{tdp_db.filename.split('.')[0]}-html.html"
+            filename_html_out = f"static/tdps/{tdp_db.id}/tdp.html"
+            os.makedirs(os.path.dirname(filename_html_out), exist_ok=True)
+            shutil.copyfile(filename_html_in, filename_html_out)
+        except Exception as e:
+            print(f"Error converting {tdp_db.filename}: {e}")
+        finally:
+            # Clean up folder
+            shutil.rmtree(f"{PDFTOHTML_FOLDER_NAME}")
 
 
 def resolve_semvers(semvers:List[Semver]) -> List[Semver]:
