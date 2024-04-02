975b087280d80462da7f4aa80682ea30ec3e91f1
Emiel Steerneman <emielsteerneman@gmail.com>
2024-04-02 09:46:48 +0200

    file manager stuff

--- data_structures/TDPName.py
+++ data_structures/TDPName.py
@@ -6,12 +6,13 @@ from .TeamName import TeamName
 
 class TDPName:
     def __init__(self, league:League, team_name:TeamName, year:int|str, index:int=0) -> TDPName:
+
         self.league:League = league
         self.team_name:TeamName = team_name
         self.year = int(year)
         self.index = index
     
-        self.filename = f"{self.league}__{self.year}__{self.team_name.name()}__{self.index}" 
+        self.filename = f"{self.league}__{self.year}__{self.team_name.name}__{self.index}" 
         
     @staticmethod
     def from_filepath(filepath:str) -> TDPName:
@@ -21,19 +22,20 @@ class TDPName:
         
     @staticmethod
     def from_string(string) -> TDPName:
+        
         fields = string.split('__')
         
         if len(fields) != 4:
             raise ValueError(f"String '{string}' does not contain 4 fields separated by '__'")
         
         league_str, year, team_str, index = fields
-        team: TeamName = TeamName(team_str)
+        team_name: TeamName = TeamName(team_str)
         league: League = League.from_string(league_str)
         
         return TDPName (
             league = league,
             year = int(year),
-            team = team,
+            team_name = team_name,
             index = int(index)
         )
     
--- managers/file_manager.py
+++ managers/file_manager.py
@@ -1,19 +1,20 @@
 # System libraries
 from abc import ABC, abstractmethod
+from azure.storage.blob import BlobServiceClient
 import os
 
 # Local libraries
-import utilities as U
 from data_structures.TDPName import TDPName
 
 class FileManager(ABC):
     
     @abstractmethod
     def store_pdf(filepath_in:str, tdp_name:TDPName, overwrite:bool=False, increment_index:bool=False):
-        """ Stores a PDF file
-        """
         raise NotImplementedError
-    
+
+    def list_pdfs(self) -> list[TDPName]:
+        raise NotImplementedError
+
     @abstractmethod
     def delete_pdf():
         raise NotImplementedError
@@ -24,10 +25,27 @@ class FileManager(ABC):
 
 class AzureFileManager(FileManager):
 
+    def __init__(self, connection_string:str):
+        self.blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+        self.container_client = self.blob_service_client.get_container_client("tdps")
+
     def store_pdf(filepath_in:str, overwrite:bool=False):
         pass
-        # azure blob bladibla
 
+    def list_pdfs(self) -> list[TDPName]:
+        pdfs = self.container_client.list_blobs()
+        pdfs = [ pdf for pdf in pdfs if pdf.name.endswith(".pdf") ]
+        # TODO remove this hack
+        pdfs = [ pdf for pdf in pdfs if "misc" not in pdf.name ]
+        pdfs = [ TDPName.from_filepath(pdf.name) for pdf in pdfs ]
+        return pdfs
+
+    def delete_pdf():
+        pass
+
+    def pdf_exists(tdp_name:TDPName):
+        pass
+    
 class LocalFileManager(FileManager):
     def __init__(self, root_dir:str):
         if not os.path.isdir(root_dir):
@@ -38,8 +56,35 @@ class LocalFileManager(FileManager):
     def store_pdf(self, filepath_in:str, tdp_name:TDPName, overwrite:bool=False, increment_index:bool=False):
         if self.pdf_exists(tdp_name) and not (overwrite or increment_index):
             raise FileExistsError(f"File {tdp_name} already exists")
-        
-    
+
+    def delete_pdf(self, tdp_name:TDPName):
+        filepath:str = os.path.join(self.root_dir, tdp_name.filename)
+        if not os.path.isfile(filepath):
+            raise FileNotFoundError(f"File {tdp_name} does not exist")
+        os.remove(filepath)
+
+    def list_pdfs(self) -> list[TDPName]:
+        pdfs = []
+        for root, dirs, files in os.walk(self.root_dir):
+            for file in files:
+                if file.endswith(".pdf"):
+                    filepath = os.path.join(root, file)
+                    # TODO remove this hack
+                    if "misc" in filepath: continue
+                    pdfs.append(TDPName.from_filepath(filepath))
+        return pdfs
+
     def pdf_exists(self, tdp_name: TDPName):
         filepath:str = os.path.join(self.root_dir, tdp_name.filename)
         return os.path.isfile(filepath)
+
+if __name__ == "__main__":
+
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    manager = LocalFileManager("tdps")
+    print(f"{len(manager.list_pdfs())} PDFs stored locally")
+
+    manager = AzureFileManager(os.getenv("AZURE_STORAGE_BLOB_TDPS_CONNECTION_STRING"))
+    print(f"{len(manager.list_pdfs())} PDFs stored in Azure Blob Storage")
