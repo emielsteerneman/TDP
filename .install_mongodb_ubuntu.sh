5e6b71345d55c5f7474959a3a0a4b60bb1eb6175
Emiel Steerneman <emielsteerneman@gmail.com>
2024-04-11 08:07:27 +0200

    mongodb

---
+++ .install_mongodb_ubuntu.sh
@@ -0,0 +1,14 @@
+# https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+
+sudo apt-get install gnupg curl
+
+curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+   --dearmor
+
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+
+sudo apt-get update
+
+sudo apt-get install -y mongodb-org
+
--- data_access/metadata/metadata_client.py
+++ data_access/metadata/metadata_client.py
@@ -1,8 +1,15 @@
 # System libraries
 import os
 import sys
+
+import pymongo.collection
+import pymongo.database
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from abc import ABC, abstractmethod
+
+# Third party libraries
+import pymongo
+from pymongo.mongo_client import MongoClient
 
 class MetadataClient(ABC):
     @abstractmethod
@@ -11,9 +18,22 @@ class MetadataClient(ABC):
     
 class CosmosDBClient(MetadataClient):
     
-    def __init__(self):
-        pass      
-    
+    def __init__(self, connection_string:str):
+        self.client = MongoClient(connection_string)
+        print(self.client)
+        print(f"Databases: {self.client.list_database_names()}")
+
     def list_tdps(self):
-        return super().list_tdps()
+        db:pymongo.database.Database = self.client.get_database("tdps")
+        col:pymongo.collection.Collection = db.get_collection("tdp")
+        entry = col.find_one()
+        print(entry)
     
+
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    client = CosmosDBClient(os.getenv("MONGODB_CONNECTION_STRING"))
+    print()
+    client.list_tdps()
