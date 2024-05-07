1b6d54e89aaaf008c79fc8c29e77a0f0c78bca7d
Emiel Steerneman <emielsteerneman@gmail.com>
2024-05-07 16:43:34 +0200

    Fixed redirect to blob storage for api/tdp on AZURE environment

--- function_app.py
+++ function_app.py
@@ -7,8 +7,9 @@ from azure import functions as func
 # Local libraries
 import app
 from data_access.metadata.metadata_client import MongoDBClient
+from data_structures.TDPName import TDPName
 from MyLogger import logger
-
+import startup
 
 load_dotenv()
 
@@ -33,18 +34,29 @@ def metadata_find(req: func.HttpRequest):
 
     json_response = json.dumps([ tdp.to_dict() for tdp in tdps ])
 
-    headers = {
-        "Cache-Control": "max-age=604800, public",
-    }
+    headers = { "Cache-Control": "max-age=604800, public" }
     
     return func.HttpResponse(json_response, mimetype="application/json", headers=headers)
 
 @azure_app.route('tdps')
 def api_tdps(req: func.HttpRequest):
     json_response:str = app.api_tdps()
-
-    headers = {
-        "Access-Control-Allow-Origin": "*"
-    }
-
+    headers = { "Access-Control-Allow-Origin": "*" }
     return func.HttpResponse(json_response, mimetype="application/json", headers=headers)
+
+@azure_app.route("tdp/{tdp_name}/pdf")
+def api_tdp_pdf(req: func.HttpRequest) -> func.HttpResponse:
+    
+    env = startup.get_environment()
+
+    if env == "LOCAL":
+        # with open(f"webapp/static/tdps/{tdp_name}.pdf", "rb") as f:
+        #     pdf_bytes = f.read()
+        # return func.HttpResponse(pdf_bytes, mimetype="application/pdf")
+        return func.HttpResponse("<h1>Not implemented</h1>", mimetype="text/html")
+
+    if env == "AZURE":
+        tdp_name:str = req.route_params.get('tdp_name')
+        tdp_name:TDPName = TDPName.from_string(tdp_name)
+        redirect_url:str = f"https://tdps.blob.core.windows.net/tdps/{tdp_name.to_filepath()}"
+        return func.HttpResponse(status_code=302, headers={"Location": redirect_url})
