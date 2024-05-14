1a0ec866d2215afc45d407d4f7fe82fbcf7402b7
Emiel Steerneman <emielsteerneman@gmail.com>
2024-05-15 01:15:35 +0200

    more script stuff

--- scripts/extract_pdf.py
+++ scripts/extract_pdf.py
@@ -11,20 +11,97 @@ from data_access.file.file_client import LocalFileClient
 from data_structures.TDPName import TDPName
 from data_structures.TDP import TDP
 from data_structures.TDPStructure import TDPStructure
+from data_structures.Paragraph import Paragraph
 from extraction import extractor
 from MyLogger import logger
-
 import startup
-# metadata_client, file_client = startup.get_clients()
+from extract_pdf_tests import paragraph_tests
 
-# pdfs = file_client.list_pdfs()
+def process(pdf_path:str):
 
-# pdf = pdfs[0]
-# pdf_path = file_client.get_pdf(pdf)
+    tdp_structure:TDPStructure = extractor.process_pdf(pdf_path)
+
+    paragraph_titles = [ paragraph.text_raw for paragraph in tdp_structure.paragraphs ]
+
+    filename = os.path.basename(pdf_path)
+
+    invalid, missing = [], []
+
+    if filename in paragraph_tests:
+        # If not equal, print the difference
+        if paragraph_titles != paragraph_tests[filename]:
+            
+            for paragraph_title in paragraph_titles:
+                if paragraph_title not in paragraph_tests[filename]:
+                    logger.error(f"Invalid paragraph '{paragraph_title}'")
+                    invalid.append(paragraph_title)
+
+            for paragraph_title in paragraph_tests[filename]:
+                if paragraph_title not in paragraph_titles:
+                    logger.error(f"Missing paragraph '{paragraph_title}'")
+                    missing.append(paragraph_title)
+        else:
+            print("Paragraphs are equal")    
+    else:
+        print(f"Missing test for {filename}")
+
+    for paragraph in tdp_structure.paragraphs:
+        print(f"{paragraph.sequence_id:3}: {paragraph.text_raw}")
+        # print(paragraph.content_raw(), "\n\n")
+
+
+    return invalid, missing
+
 
 pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/smallsize/2023/soccer_smallsize__2023__RoboTeam_Twente__0.pdf"
-# pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/rescue/robot/2022/rescue_robot__2022__BART_LAB__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/athome/domestic/2019/athome_domestic__2019__Austin_Villa__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/midsize/2010/soccer_midsize__2010__IsePorto__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/smallsize/2009/soccer_smallsize__2009__B-Smart__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/humanoid/kid/2019/soccer_humanoid_kid__2019__Ichiro__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/humanoid/teen/2019/soccer_humanoid_teen__2019__MRL__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/industrial/atwork/2021/industrial_atwork__2021___AutonOHM__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/simulation/2d/2022/soccer_simulation_2d__2022__FRA-UNIted__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/soccer/humanoid/adult/2019/soccer_humanoid_adult__2019__Tsinghua_Hephaestus__0.pdf"
+pdf_path = "/home/emiel/Desktop/projects/tdp/static/pdf/industrial/logistics/2019/industrial_logistics__2019__Solidus__0.pdf"
 
-processed = extractor.process_pdf(pdf_path)
+results = {}
 
-print(processed.outline())
+if False:
+    # os.system(f"xdg-open {pdf_path}")
+    invalid, missing = process(pdf_path)
+    results[pdf_path] = {
+        "invalid": invalid,
+        "missing": missing
+    }
+else:
+    for filename in paragraph_tests:
+        print("\n\n")
+        print(filename)
+        tdpname = TDPName.from_filepath(filename)
+        filepath = f"/home/emiel/Desktop/projects/tdp/static/pdf/{tdpname.to_filepath()}"
+        invalid, missing = process(filepath)
+        results[filename] = {
+            "invalid": invalid,
+            "missing": missing
+        }
+
+for filename in results:
+    print()
+    print(os.path.basename(filename))
+
+    if os.path.basename(filename) not in paragraph_tests:
+        print("No tests found for this file")
+        continue
+
+    invalid, missing = results[filename]["invalid"], results[filename]["missing"]
+    if len(invalid) > 0:
+        print("Invalid:")
+        for paragraph in invalid:
+            print(f"  '{paragraph}'")
+    if len(missing) > 0:
+        print("Missing:")
+        for paragraph in missing:
+            print(f"  '{paragraph}'")
+
+    if len(invalid) == 0 and len(missing) == 0:
+        print("All tests passed")
--- scripts/fill_mongodb_local.py
+++ scripts/fill_mongodb_local.py
@@ -8,26 +8,84 @@ load_dotenv()
 # Local libraries
 from data_access.metadata.metadata_client import MongoDBClient
 from data_access.file.file_client import LocalFileClient
+from data_access.vector.weaviate_client import WeaviateClient
 from data_structures.TDPName import TDPName
 from data_structures.TDP import TDP
 from data_structures.TDPStructure import TDPStructure
+from embedding.Embeddings import instance as embeddor
 from extraction import extractor
 from MyLogger import logger
 
-metadata_client:MongoDBClient = MongoDBClient("mongodb://localhost:27017/")
+weaviate_client = WeaviateClient.default_client()
+weaviate_client.reset_everything()
+
 file_client:LocalFileClient = LocalFileClient(os.getenv("LOCAL_FILE_ROOT"))
+# metadata_client:MongoDBClient = MongoDBClient("mongodb://localhost:27017/")
 
-metadata_client.drop_tdps()
+# metadata_client.drop_tdps()
+# metadata_client.drop_paragraphs()
 
-pdfs = file_client.list_pdfs()
-print(f"Found len(pdfs) PDFs")
+pdfs:list[TDPName] = file_client.list_pdfs()[:1]
+
+# pdfs = [ _ for _ in pdfs if "RoboTeam_Twente" in _.filename ]
+
+print(f"Found {len(pdfs)} PDFs")
+
+total_tokens:int = 0
+total_n_tokens = []
+
 for i_pdf, pdf in enumerate(pdfs):
-    print(f"Processing PDF {i_pdf+1}/{len(pdfs)}")
-    pdf_filepath = file_client.get_pdf(pdf)
+    print("\n\n\n")
+    print(f"Processing PDF {i_pdf+1:3}/{len(pdfs)} : {pdf}")
+    pdf_filepath = file_client.get_pdf(pdf, no_copy=True)
     pdf_filehash = file_client.get_pdf_hash(pdf)
-    # tdp_structure:TDPStructure = extractor.process_pdf(pdf_filepath)
-    tdp_structure = None
+    tdp_structure:TDPStructure = extractor.process_pdf(pdf_filepath)
 
-    print(tdp_structure)
     tdp = TDP(tdp_name=pdf, filehash=pdf_filehash, structure=tdp_structure)
+    tdp.propagate_information()
+
+
+    for paragraph in tdp.structure.paragraphs[:6]:
+        print(f"Paragraph title: {paragraph.text_raw:30} {len(paragraph.content_raw().split(' ')):3} words")
+        # print(f"Paragraph content: '{paragraph.content_raw()}'")
+
+        n_tokens = embeddor.count_tokens(paragraph.content_raw())
+        costs = embeddor.get_price_per_token("text-embedding-3-small") * n_tokens
+        
+        total_tokens += n_tokens
+        total_n_tokens.append(n_tokens)
+        # print(f"Costs: {costs:.2f}")
+
+        text = paragraph.text_processed + " " + paragraph.content_processed()
+        embedding = embeddor.embed_using_openai(text, model="text-embedding-3-small")
+
+        weaviate_client.store_paragraph(paragraph, embedding)
+
+
+
+    continue
+    print(tdp_structure.outline())
+
+    tdp = TDP(tdp_name=pdf, filehash=pdf_filehash, structure=tdp_structure)
+    tdp.propagate_information()
+
     metadata_client.insert_tdp(tdp)
+
+    for paragraph in tdp.structure.paragraphs:
+        metadata_client.insert_paragraph(paragraph)
+
+
+weaviate_client.search_paragraphs_by_embedding(embedding, limit=5)
+
+# print(f"Total tokens: {total_tokens}")
+# print(f"Total costs for text-embedding-3-small: {total_tokens * embeddor.get_price_per_token('text-embedding-3-small'):.2f}")
+# print(f"Total costs for     gpt-3.5-turbo-0125: {total_tokens * embeddor.get_price_per_token('gpt-3.5-turbo-0125'):.2f}")
+# print(f"Total costs for                  gpt-4: {total_tokens * embeddor.get_price_per_token('gpt-4'):.2f}")
+# print(f"Total costs for                 gpt-4o: {total_tokens * embeddor.get_price_per_token('gpt-4o'):.2f}")
+
+# print(f"Average tokens per paragraph: {total_tokens / len(total_n_tokens)}")
+# print(f"Median tokens per paragraph: {sorted(total_n_tokens)[len(total_n_tokens)//2]}")
+# print(f"Max tokens per paragraph: {max(total_n_tokens)}")
+# print(f"Min tokens per paragraph: {min(total_n_tokens)}")
+# from collections import Counter
+# print(Counter(total_n_tokens).most_common(10))
