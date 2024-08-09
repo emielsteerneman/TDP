c118ccc9ba33c9a5ac9f218012a8cac8b60e1021
Emiel Steerneman <emielsteerneman@gmail.com>
2024-08-10 00:11:48 +0200

    updated blacklist

--- blacklist.py
+++ blacklist.py
@@ -1,3 +1,6 @@
 blacklist = {}
 
 blacklist['soccer_smallsize__2012__RoboJackets__0'] = "Full of weird spaces, breaking up words and making it impossible to process"
+blacklist['soccer_simulation_2d__2006__Mersad__0'] = "PyMUpdf can not extract text from this PDF at all. Complete garbage."
+blacklist['soccer_simulation_2d__2007__WrightEagle__0'] = "PyMUpdf can not extract text from this PDF at all. Complete garbage."
+blacklist['soccer_simulation_2d__2004__CM(Loki)__0'] = "PyMUpdf can not extract text from this PDF at all. Complete garbage."
